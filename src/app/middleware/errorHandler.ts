6d3dd629e70243da46253c2f9d74bdc538b22bbf
SangBV <mfvansang@gmail.com>
2021-01-20 23:35:56 +0700

    [Add] Apply winston to error handler

--- src/app/middleware/errorHandler.ts
+++ src/app/middleware/errorHandler.ts
@@ -1,8 +1,31 @@
-import { errorResponse } from './../helpers/response';
+import { errorResponse } from '../helpers/response';
 import { COMMON_MESSAGE } from '../utils/constant';
+import { transports, createLogger, format } from 'winston';
+import * as moment from 'moment';
+
+// Logger configuration
+const winstonLogConfig = {
+    level: 'error',
+    format: format.combine(
+        format.timestamp(),
+        format.json()
+    ),
+    'transports' : [
+        new transports.File({
+            level: 'info',
+            filename: './assets/logs/test-winston.log'
+        })
+    ]
+};
 
 export function errorHandler(err, req, res, next) {
     if(err) {
+        // Create the logger
+        var logger = createLogger(winstonLogConfig);
+
+        // Log a message
+        logger.error(err);
+
         let errorMessgae = err.message ? err.message : COMMON_MESSAGE.ERROR_PROCESSING;
         return errorResponse(req, res, errorMessgae, 500, err);
     }
